390ac7bcd46755d38ef4e07292b6a0512254e560
Jatin Kumar <jatink220@gmail.com>
2021-09-03 22:37:45 +0530

    Parenthesis matching program added

---
+++ Code/C/09_ParenthesisMatchingInStack.c
@@ -0,0 +1,75 @@
+// Program for parenthesis matching that is the string is balance or not
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include <limits.h>
+
+struct stack{
+    int size;
+    int top;
+    char *arr;
+};
+
+bool isEmpty(struct stack *stack){
+    if(stack->top == -1){
+        return true;
+    }
+    return false;
+}
+
+bool isFull(struct stack *stack){
+    if(stack->top == stack->size - 1){
+        return true;
+    }
+    return false;
+}
+
+void push(struct stack *stack, char element){
+    if(isFull(stack)){
+        printf("Stack Overflow!\n");
+        return;
+    }
+    stack->top++;
+    stack->arr[stack->top] = element;
+}
+
+char pop(struct stack *stack){
+    if(isEmpty(stack)){
+        printf("Stack Underflow!\n");
+        return -1;
+    }
+    char val = stack->arr[stack->top];
+    stack->top--;
+    return val;
+}
+
+char * parenthesis_match(char * exp){
+    struct stack *st = (struct stack * ) malloc(sizeof(struct stack));
+    st->top = -1;
+    st->size = sizeof(exp);
+    st->arr = (char *) malloc(sizeof(st->size * sizeof(char)));
+    
+    for(int i = 0; exp[i] != '\0'; i++){
+        if(exp[i] == '(')
+            push(st, '(');
+        else if(exp[i] == ')'){
+            if(isEmpty(st))
+            return "Unbalanced";
+            pop(st);
+        }
+    }
+
+    if(isEmpty(st))
+        return "Balanced";
+    return "Unbalanced";
+}
+
+int main()
+{   
+    char * exp;
+    scanf("%c", &exp);
+    char * res = parenthesis_match(exp);
+    printf("%s", res);
+    return 0;
+}
