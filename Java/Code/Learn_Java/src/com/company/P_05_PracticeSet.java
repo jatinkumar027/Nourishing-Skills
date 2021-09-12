1feb84800c9401d0a2c1780f8ff2c74532c19f53
Jatin Kumar <jatink220@gmail.com>
2021-09-12 17:26:39 +0530

    Practice Set

--- Java/Code/Learn_Java/src/com/company/P_05_PracticeSet.java
+++ Java/Code/Learn_Java/src/com/company/P_05_PracticeSet.java
@@ -1,4 +1,16 @@
 package com.company;
-
+import java.util.Scanner;
+// Deteting the number is integer is not
 public class P_05_PracticeSet {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        boolean b1 = sc.hasNextInt();
+        if(b1){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
+    }
 }
