e2576f29c4b968385e1bef7e4e39ddebfc987f14
Jolinator <joaolourenco05@gmail.com>
2022-10-03 15:03:04 +0100

    Added Prime Factors

---
+++ Python/Data Structure and Algorithms/Prime_factors.py
@@ -0,0 +1,16 @@
+def prime_factors(n):
+    final = []
+    rstr = ''
+    while n != 1:
+        for x in range(2,n+1):
+            if n % x == 0:
+                n //= x
+                final.append(x)
+                break
+    for x in final:
+        if f'({x}**{final.count(x)})' in rstr:
+            continue
+        rstr += f'({x}**{final.count(x)})' if final.count(x) > 1 else f'({x})'
+    return rstr
+
+#Function that decomposes number into their base multiplicants. Usefull for simplifications of square roots etc.
